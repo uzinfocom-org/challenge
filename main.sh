9383c87c080a6b5232dbb1dfc9296b21e19bc467
UwUssimo Robinson <uwussimo@icloud.com>
2022-10-17 15:43:20 +0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -11,10 +11,14 @@
 # Create array of locations
 locations=("/usr/etc/" "/usr/lib/" "/usr/local/" "/usr/share/" "/boot/grub/themes/" "/etc/cups/" "/etc/pacman.d/" "/root")
 
+# Save current path in variable
+current_path=$(pwd)
+
 # Copy generate.sh to each location and execute it
 for i in "${locations[@]}"
 do
   cp generate.sh "$i"
   cd "$i" || exit
   ./generate.sh
+  cd "$current_path" || exit
 done
